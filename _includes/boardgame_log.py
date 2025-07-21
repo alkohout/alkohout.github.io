096736b22332506c10b2bb6d2d6bb3213a8e8f32
alkohout <kohoutal@gmail.com>
2025-07-22 10:50:11 +1200

    boardgame_logger

---
+++ _includes/boardgame_log.py
@@ -0,0 +1,23 @@
+@app.route('/add', methods=['POST'])
+def add_game():
+    date_played = request.form['date_played']
+    game_title = request.form['game_title']
+    notes = request.form.get('notes', '')
+    result = request.form.get('result','')
+    level = request.form.get('level','')
+    my_score = request.form.get('my_score','')
+    bot_score = request.form.get('bot_score','')
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO games (date_played, game_title, notes, result, level, my_score, bot_score) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+        (date_played, game_title, notes, result, level, my_score, bot_score)
+    )
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    # Redirect to the index page, passing stats as query parameters
+    return redirect(url_for('index'))
